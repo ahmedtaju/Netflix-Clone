bb0572f66d16174d170d69b6c330bab17e89c51d
ahmedtaju <ahmmedahmd02@gmail.com>
2025-05-20 00:55:02 +0300

    final product

--- netflix-clone/src/components/Header/Navbar.jsx
+++ netflix-clone/src/components/Header/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Netflix from "../../assets/_netflix.png";
+import React, { useEffect, useState } from "react";
+import NetflixLogo from "../../assets/_netflix.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
@@ -7,34 +7,36 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import "./header.css";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShow(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="navbar">
-      <div className="container">
-        <div className="left">
-          <img src={Netflix} alt="netflix-logo" />
-          <ul>
+    <div className={`navbar ${show ? "navbar--black" : ""}`}>
+      <div className="navbar__container">
+        <div className="navbar__left">
+          <img src={NetflixLogo} alt="Netflix Logo" className="navbar__logo" />
+          <ul className="navbar__links">
             <li>Home</li>
+            <li>TV Shows</li>
             <li>Movies</li>
-            <li>Series</li>
-            <li>News and Popular</li>
+            <li>New & Popular</li>
             <li>My List</li>
+            <li>Browse by Languages</li>
           </ul>
         </div>
-        <div className="right">
-          <ul>
-            <li>
-              <SearchIcon />
-            </li>
-            <li>
-              <NotificationsNoneIcon />
-            </li>
-            <li>
-              <AccountBoxIcon />
-            </li>
-            <li>
-              <ArrowDropDownIcon />
-            </li>
-          </ul>
+        <div className="navbar__right">
+          <SearchIcon className="navbar__icon" />
+          <NotificationsNoneIcon className="navbar__icon" />
+          <AccountBoxIcon className="navbar__icon" />
+          <ArrowDropDownIcon className="navbar__icon" />
         </div>
       </div>
     </div>
---
+++ netflix-clone/src/components/row/Row.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import axios from "../../util/axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(""); // Close if clicked again
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log("Trailer not found", error));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                key={movie.id}
+                className={`row__poster ${
+                  isLargeRow ? "row__posterLarge" : ""
+                }`}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name || movie.title}
+                onClick={() => handleClick(movie)}
+              />
+            )
+        )}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+};
+
+export default Row;
